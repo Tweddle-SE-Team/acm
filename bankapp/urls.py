486207b789ce6834e64fb59e3b4e16a472772a11
Justin Dickow <jdickow@tweddle.com>
2017-09-03 19:29:00 -0400

    add the other pages

--- bankapp/urls.py
+++ bankapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^login/', views.login, name='login'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^account/', views.account, name='account'),
     url(r'^$', views.index, name='index')
 ]
--- bankapp/views.py
+++ bankapp/views.py
@@ -3,11 +3,20 @@ from django.template import loader
 
 from .models import Faq
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the bankapps index.")
+    return login(request)
 
 def login(request):
     template = loader.get_template('bankapp/login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def register(request):
+    template = loader.get_template('bankapp/register.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def account(request):
+    template = loader.get_template('bankapp/account.html')
     context = {}
     return HttpResponse(template.render(context, request))
