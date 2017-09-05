9e0279d687a6a1d86842adbb9e3e7d4c677e2b29
Justin Dickow <jdickow@tweddle.com>
2017-09-05 08:35:55 -0400

    gitignore

--- bankapp/urls.py
+++ bankapp/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/', views.login, name='login'),
-    url(r'^register/', views.register, name='register'),
-    url(r'^account/', views.account, name='account'),
+    url(r'^login', views.login, name='login'),
+    url(r'^register', views.register, name='register'),
+    url(r'^account', views.account, name='account'),
     url(r'^$', views.index, name='index')
 ]
